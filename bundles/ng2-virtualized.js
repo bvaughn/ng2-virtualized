5ec8cf0c6e38d004ef1d9ea06bfa818902d78663
Brian Vaughn <brian.david.vaughn@gmail.com>
2016-04-05 22:10:01 -0700

    Removed bundles folder

--- bundles/ng2-virtualized.js
+++
@@ -1,7 +0,0 @@
-System.registerDynamic("ng2-virtualized", [], false, function($__require, $__exports, $__module) {
-  var _retrieveGlobal = System.get("@@global-helpers").prepareGlobal($__module.id, null, null);
-  (function() {
-    "use strict";
-  })();
-  return _retrieveGlobal();
-});
